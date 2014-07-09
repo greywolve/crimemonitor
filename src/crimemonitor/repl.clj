fd44c4060982d7b3e4fece27495ed98580d10dff
Oliver Powell <oliverpowell84@gmail.com>
2014-07-09 18:16:14 +0200

    Reloaded workflow working, but had to ignore check-for-errors! This seems to be because trapperkeeper in our project.clj is not the latest version.

--- src/crimemonitor/repl.clj
+++ src/crimemonitor/repl.clj
@@ -1,1 +1,48 @@
-(ns crimemonitor.repl)
+(ns crimemonitor.repl
+  (:require [puppetlabs.trapperkeeper.services.webserver.jetty9-service
+              :refer [jetty9-service]]
+            [crimemonitor.services.core :refer [simple-service datomic-service hello-web-service]]
+            [puppetlabs.trapperkeeper.core :as tk]
+            [puppetlabs.trapperkeeper.app :as tka]
+            [clojure.tools.namespace.repl :refer (refresh)]))
+
+(def system nil)
+
+(defn init []
+  (alter-var-root #'system
+                  (constantly (tk/build-app
+                               [jetty9-service
+                                simple-service
+                                hello-web-service
+                                datomic-service]
+                               {:global {:logging-config "test-resources/logback-dev.xml"}
+                                :webserver {:host "localhost"
+                                            :port 8080 }
+                                :datomic   {:uri
+                                            "datomic:free://localhost:4334/crimemonitor"}
+                                :crimemonitor {:url-prefix "/crimemonitor"}})))
+  (alter-var-root #'system tka/init)
+  #_(tka/check-for-errors! system))
+
+(defn start []
+  (alter-var-root #'system
+                  (fn [s] (if s (tka/start s))))
+  #_(tka/check-for-errors! system))
+
+(defn stop []
+  (alter-var-root #'system
+                  (fn [s] (if s (tka/stop s)))))
+
+(defn go []
+  (init)
+  (start))
+
+(defn context []
+  @(tka/app-context system))
+
+(defn print-context []
+  (clojure.pprint/pprint (context)))
+
+(defn reset []
+  (stop)
+  (refresh :after 'examples.ring-app.repl/go))
