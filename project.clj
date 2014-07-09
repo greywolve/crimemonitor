5e8da33cf5a6d26a95a27546416b500ff69d058a
Oliver Powell <oliverpowell84@gmail.com>
2014-07-09 18:26:28 +0200

    Update trapperkeeper, kitchensink, and the jetty webservice to the latest versions.

--- project.clj
+++ project.clj
@@ -9,21 +9,21 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [puppetlabs/trapperkeeper "0.3.4"]
+                 [puppetlabs/trapperkeeper "0.4.2"]
                  [com.datomic/datomic-free "0.9.4815.12"
                   :exclusions [org.clojure/tools.cli
                                org.slf4j/slf4j-nop]]
                  [datomic-schema "1.1.0"]
                  [org.clojure/test.check "0.5.8"]
-                 [org.clojure/tools.namespace "0.2.4"]]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.2"]]
 
   :test-paths ["test" "test-resources"]
 
-  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper-webserver-jetty9 "0.3.3"]
-                                  [puppetlabs/trapperkeeper "0.3.4"
+  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper "0.4.2"
                                    :classifier "test"
                                    :scope "test"]
-                                  [puppetlabs/kitchensink "0.5.3"
+                                  [puppetlabs/kitchensink "0.7.1"
                                    :classifier "test"
                                    :scope "test"]
                                   [clj-http "0.7.9"]
